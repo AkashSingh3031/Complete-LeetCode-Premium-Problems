3f4ce4cb86fce6733f2d456d52c21aa6480d6ff3
Akash Singh <akashsingh27101998@gmail.com>
2023-07-07 18:58:00 +0530

    Time: 6 ms (43.73%), Space: 8.2 MB (38.16%) - LeetHub

---
+++ 0249-group-shifted-strings/0249-group-shifted-strings.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<string>> groupStrings(vector<string>& strings) {
+        unordered_map<string, vector<string> > mp;
+        for (string  s : strings)
+            mp[shift(s)].push_back(s);
+        vector<vector<string> > groups;
+        for (auto m : mp) {
+            vector<string> group = m.second;
+            sort(group.begin(), group.end());
+            groups.push_back(group);
+        }
+        return groups;
+    }
+private:
+    string shift(string& s) {
+        string t;
+        int n = s.length();
+        for (int i = 1; i < n; i++) {
+            int diff = s[i] - s[i - 1];
+            if (diff < 0) diff += 26;
+            t += 'a' + diff + ',';
+        }
+        return t;
+    }
+};
