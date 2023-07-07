ed369ee7fb543a1f192f6be229fbf3cf65aeced8
Akash Singh <akashsingh27101998@gmail.com>
2023-07-07 18:50:20 +0530

    Time: 0 ms (100%), Space: 6 MB (44.34%) - LeetHub

---
+++ 0246-strobogrammatic-number/0246-strobogrammatic-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isStrobogrammatic(string num) {
+        unordered_map<char, char> lut{{'0', '0'}, {'1', '1'}, {'6', '9'}, {'8', '8'}, {'9', '6'}};
+        for (int l = 0, r = num.size() - 1; l <= r; l++, r--) {
+            if (lut.find(num[l]) == lut.end() || lut[num[l]] != num[r]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
