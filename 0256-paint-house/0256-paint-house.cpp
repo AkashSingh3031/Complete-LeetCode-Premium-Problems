8328731ba8babac239988cfbb17c9c78ea5e8623
Akash Singh <akashsingh27101998@gmail.com>
2023-07-07 19:23:59 +0530

    Time: 9 ms (67.64%), Space: 9.4 MB (100%) - LeetHub

---
+++ 0256-paint-house/0256-paint-house.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minCost(vector<vector<int>>& costs) {
+        
+    int n = costs.size();
+    for (int i = 1; i < n; i++) {
+        costs[i][0] += min(costs[i - 1][1], costs[i - 1][2]);
+        costs[i][1] += min(costs[i - 1][0], costs[i - 1][2]);
+        costs[i][2] += min(costs[i - 1][0], costs[i - 1][1]);
+    }
+    return (n == 0) ? 0 : (min(costs[n - 1][0], min(costs[n - 1][1], costs[n - 1][2])));
+    }
+};
