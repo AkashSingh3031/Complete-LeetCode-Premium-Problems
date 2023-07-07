737d2e844c4c3a2bec3d9cafd40c3836d7ab1886
Akash Singh <akashsingh27101998@gmail.com>
2023-07-07 18:40:23 +0530

    Time: 6 ms (14.45%), Space: 6.7 MB (29.35%) - LeetHub

---
+++ 0161-one-edit-distance/0161-one-edit-distance.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isOneEditDistance(string s, string t) {
+        int m = s.size(), n = t.size();
+        if (m > n) {
+            return isOneEditDistance(t, s);
+        }
+        for (int i = 0; i < m; i++) {
+            if (s[i] != t[i]) {
+                if (m == n) {
+                    return s.substr(i + 1) == t.substr(i + 1);
+                }
+                return s.substr(i) == t.substr(i + 1);
+            }
+        }
+        return m + 1 == n;
+    }
+};
