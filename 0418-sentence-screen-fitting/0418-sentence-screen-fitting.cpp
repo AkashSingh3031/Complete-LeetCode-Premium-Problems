a36a967c68cb11bb3071cc86f6d0fa013b46f959
Akash Singh <akashsingh27101998@gmail.com>
2023-07-09 22:59:13 +0530

    Time: 27 ms (50.35%), Space: 7.3 MB (80.14%) - LeetHub

---
+++ 0418-sentence-screen-fitting/0418-sentence-screen-fitting.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int wordsTyping(vector<string>& sentence, int rows, int cols) {
+        unordered_map<int, int> umap;
+        int num = 0, n = sentence.size();
+        for(int i = 0; i < rows; i++){
+            int start = num % n;
+            if(umap.count(start) == 0){
+                int cnt = 0, len = 0;
+                for(int i = start; len < cols; i = (i+1) % n, cnt++){
+                    if(len + sentence[i].size() > cols)
+                        break;
+                    len += sentence[i].size() + 1;
+                }
+                num += cnt;
+                umap.emplace(start, cnt);
+            }
+            else
+                num += umap[start];
+        }
+        return num / n;
+    }
+};
