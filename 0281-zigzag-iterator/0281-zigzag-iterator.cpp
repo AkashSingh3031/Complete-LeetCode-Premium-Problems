538640d09751209ff218f70229462ce737e76eda
Akash Singh <akashsingh27101998@gmail.com>
2023-07-09 22:33:04 +0530

    Time: 9 ms (37.36%), Space: 8.3 MB (20.12%) - LeetHub

---
+++ 0281-zigzag-iterator/0281-zigzag-iterator.cpp
@@ -0,0 +1,30 @@
+class ZigzagIterator {
+public:
+    ZigzagIterator(vector<int>& v1, vector<int>& v2) {
+        if (v1.size() != 0)
+            Q.push(make_pair(v1.begin(), v1.end()));
+        if (v2.size() != 0)
+            Q.push(make_pair(v2.begin(), v2.end()));
+    }
+
+    int next() {
+        vector<int>::iterator it = Q.front().first;
+        vector<int>::iterator endIt = Q.front().second;
+        Q.pop();
+        if (it + 1 != endIt)
+            Q.push(make_pair(it+1, endIt));
+        return *it;
+    }
+
+    bool hasNext() {
+        return !Q.empty();
+    }
+private:
+    queue<pair<vector<int>::iterator, vector<int>::iterator>> Q;
+};
+
+/**
+ * Your ZigzagIterator object will be instantiated and called as such:
+ * ZigzagIterator i(v1, v2);
+ * while (i.hasNext()) cout << i.next();
+ */
