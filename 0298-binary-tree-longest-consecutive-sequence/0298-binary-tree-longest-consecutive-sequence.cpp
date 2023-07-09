f51816d7f177a82cb75e3898d01b066ec0828e5c
Akash Singh <akashsingh27101998@gmail.com>
2023-07-09 22:38:19 +0530

    Time: 42 ms (51.86%), Space: 32.4 MB (53.83%) - LeetHub

---
+++ 0298-binary-tree-longest-consecutive-sequence/0298-binary-tree-longest-consecutive-sequence.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int longestConsecutive(TreeNode* root) {
+        return search(root, nullptr, 0);
+    }
+    
+    int search(TreeNode *root, TreeNode *parent, int len) {
+        if (!root) return len;
+        len = (parent && root->val == parent->val + 1)?len+1:1;
+        return max(len, max(search(root->left, root, len), search(root->right, root, len)));
+    }
+};
