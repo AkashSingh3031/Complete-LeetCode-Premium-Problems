1ca072639db87b3517045271935eac02986b061a
Akash Singh <akashsingh27101998@gmail.com>
2023-07-09 22:59:03 +0530

    Time: 3 ms (100%), Space: 8.2 MB (60%) - LeetHub

---
+++ 0411-minimum-unique-word-abbreviation/0411-minimum-unique-word-abbreviation.cpp
@@ -0,0 +1,61 @@
+class Solution {
+    int n, cand, bn, minlen, minab;
+    vector<int> dict;
+    
+    // Return the length of abbreviation given bit sequence
+    int abbrLen(int mask) {
+        int count = n;
+        for (int b = 3; b < bn; b <<= 1)
+            if ((mask & b) == 0)
+                count --;
+        return count;
+    }
+
+    // DFS backtracking
+    void dfs(int bit, int mask) {
+        int len = abbrLen(mask);
+        if (len >= minlen) return;
+        bool match = true;
+        for (auto d : dict) {
+            if ((mask & d) == 0) {
+                match = false;
+                break;
+            }
+        }
+        if (match) {
+            minlen = len;
+            minab = mask;
+        }
+        else
+            for (int b = bit; b < bn; b <<= 1)
+                if (cand & b) dfs(b << 1, mask + b);
+    }
+
+public:
+    string minAbbreviation(string target, vector<string>& dictionary) {
+        n = target.size(), bn = 1 << n, cand = 0, minlen = INT_MAX;
+        string res;
+        
+        // Preprocessing with bit manipulation
+        for (auto w : dictionary) {
+            int word = 0;
+            if (w.size() != n) continue;
+            for (int i = n-1, bit = 1; i >= 0; --i, bit <<= 1)
+                if (target[i] != w[i]) word += bit;
+            dict.push_back(word);
+            cand |= word;
+        }
+        dfs(1, 0);
+
+        // Reconstruct abbreviation from bit sequence
+        for (int i = n-1, pre = i; i >= 0; --i, minab >>= 1) {
+            if (minab & 1) {
+                if (pre-i > 0) res = to_string(pre-i) + res;
+                pre = i - 1;
+                res = target[i] + res;
+            }
+            else if (i == 0) res = to_string(pre-i+1) + res;
+        }
+        return res;
+    }
+};
