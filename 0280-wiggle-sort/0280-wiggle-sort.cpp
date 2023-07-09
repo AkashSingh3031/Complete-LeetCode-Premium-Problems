b91b2dfe31e13e8ecf6924c107dcb9787b63a0d5
Akash Singh <akashsingh27101998@gmail.com>
2023-07-09 22:32:54 +0530

    Time: 12 ms (59.21%), Space: 13.4 MB (79.77%) - LeetHub

---
+++ 0280-wiggle-sort/0280-wiggle-sort.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 1; i < n; i++)
+            if (((i & 1) && nums[i] < nums[i - 1]) || (!(i & 1) && nums[i] > nums[i - 1]))
+                swap(nums[i], nums[i - 1]);
+    }
+};
